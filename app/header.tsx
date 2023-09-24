b20b895224e2429bc790b2f446423acbb2b33cf8
shaneab30 <shaneardyanto@gmail.com>
2023-09-24 10:36:36 +0700

    added search bar and updates

--- app/header.tsx
+++ app/header.tsx
@@ -20,12 +20,17 @@ import { firebaseApp, getUserData } from "./firebase";
 import Link from "next/link";
 import { useAppSelector } from "@/redux/hooks";
 import { useRouter } from "next/navigation";
+import StoreIcon from '@mui/icons-material/Store';
+import { styled, alpha } from '@mui/material/styles';
+import { InputBase } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import MoreIcon from '@mui/icons-material/MoreVert';
 
 interface HeaderProps {
 
 }
 
-const pages = ['Home', 'Pricing', 'Blog'];
+const pages = ['Home', 'Add Product'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 
@@ -51,8 +56,23 @@ const Header: FunctionComponent<HeaderProps> = () => {
         setAnchorElUser(null);
     };
 
-    const handleClickUserMenu = (key:string) => {
-        switch(key){
+    const handleClickNavMenu = (key: string) => {
+        switch (key) {
+            case 'Home':
+                router.push('/');
+                break;
+            case 'Add Product':
+                router.push('/store/addproduct');
+                break;
+            default:
+                console.log(`Handler function for ${key} not found`);
+                break;
+        }
+        setAnchorElNav(null);
+    };
+
+    const handleClickUserMenu = (key: string) => {
+        switch (key) {
             case 'Profile':
                 router.push('/profile');
                 break;
@@ -65,13 +85,53 @@ const Header: FunctionComponent<HeaderProps> = () => {
         }
     }
 
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    }));
+
+    const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }));
+
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        '& .MuiInputBase-input': {
+            padding: theme.spacing(1, 1, 1, 0),
+            // vertical padding + font size from searchIcon
+            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('md')]: {
+                width: '20ch',
+            },
+        },
+    }));
+
     const currentUser = useAppSelector(state => state.user.currentUser);
 
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <StoreIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
                     <Typography
                         variant="h6"
                         noWrap
@@ -87,7 +147,7 @@ const Header: FunctionComponent<HeaderProps> = () => {
                             textDecoration: 'none',
                         }}
                     >
-                        LOGO
+                        MarketHub
                     </Typography>
 
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -119,14 +179,24 @@ const Header: FunctionComponent<HeaderProps> = () => {
                                 display: { xs: 'block', md: 'none' },
                             }}
                         >
+                            <Search>
+                                <SearchIconWrapper>
+                                    <SearchIcon />
+                                </SearchIconWrapper>
+                                <StyledInputBase
+                                    placeholder="Search…"
+                                    inputProps={{ 'aria-label': 'search' }}
+                                />
+                            </Search>
                             {pages.map((page) => (
-                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                <MenuItem key={page} onClick={() => handleClickNavMenu(page)}>
                                     <Typography textAlign="center">{page}</Typography>
+
                                 </MenuItem>
                             ))}
                         </Menu>
                     </Box>
-                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <StoreIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
                     <Typography
                         variant="h5"
                         noWrap
@@ -143,25 +213,36 @@ const Header: FunctionComponent<HeaderProps> = () => {
                             textDecoration: 'none',
                         }}
                     >
-                        LOGO
+                        MarketHub
                     </Typography>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                    <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
                         {pages.map((page) => (
                             <Button
                                 key={page}
-                                onClick={handleCloseNavMenu}
+                                onClick={() => handleClickNavMenu(page)}
                                 sx={{ my: 2, color: 'white', display: 'block' }}
                             >
                                 {page}
                             </Button>
                         ))}
                     </Box>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        <Search>
+                            <SearchIconWrapper>
+                                <SearchIcon />
+                            </SearchIconWrapper>
+                            <StyledInputBase
+                                placeholder="Search…"
+                                inputProps={{ 'aria-label': 'search' }}
+                            />
+                        </Search>
+                    </Box>
 
                     <Box sx={{ flexGrow: 0 }}>
                         {
                             currentUser ? <Tooltip title="Open settings">
                                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    <Avatar alt={currentUser.name.toUpperCase()} src="/static/images/avatar/1.jpg"/>
+                                    <Avatar alt={currentUser.name.toUpperCase()} src="/static/images/avatar/1.jpg" />
                                 </IconButton>
                             </Tooltip>
                                 :
@@ -188,7 +269,7 @@ const Header: FunctionComponent<HeaderProps> = () => {
                             onClose={handleCloseUserMenu}
                         >
                             {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={()=> handleClickUserMenu(setting)}>
+                                <MenuItem key={setting} onClick={() => handleClickUserMenu(setting)}>
                                     <Typography textAlign="center">{setting}</Typography>
                                 </MenuItem>
                             ))}
